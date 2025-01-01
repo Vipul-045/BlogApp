cc2de63d67b7086f76a8ff0ab3e392b18be3e2a3
Vipul Yadav <vipulyadav0479@gmail.com>
2025-01-01 22:12:30 +0530

    Appbar modsification

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -3,8 +3,8 @@ import { Avatar } from "./BlogCard"
 function AppBar() {
   return (
     <div className="border-b-2 flex justify-between px-10 py-3">
-        <div className="text-xl font-semibold">Medium</div>
-        <div className=""><Avatar name="Vipul"/></div>
+        <div className="text-xl font-semibold flex flex-col justify-center">Medium</div>
+        <div className=""><Avatar size="big" name="Vipul"/></div>
     </div>
   )
 }
--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -11,7 +11,7 @@ function BlogCard({
     <div className="p-4 border-b border-slate-400 pb-4">
       <div className="flex">
         <div>
-          <Avatar name={authorName} />
+          <Avatar size="small" name={authorName} />
         </div>
         <div className="font-extralight px-2">{authorName}</div>
         <div className="flex justify-center flex-col"><Circle/></div>
@@ -28,9 +28,9 @@ function Circle(){
   </div>
 }
 
-export function Avatar({ name }: { name: string }) {
-  return <div className="relative inline-flex items-center justify-center w-7 h-7 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-800">
-    <span className="font-small text-gray-600 dark:text-gray-300">{name[0]}</span>
+export function Avatar({ name, size = "small" }: { name: string, size: "small" | "big" }, ) {
+  return <div className={`relative inline-flex items-center justify-center overflow-hidden bg-gray-100 rounded-full dark:bg-gray-800 ${size === "small" ? "w-6 h-6" : "w-10 h-10"}`}>
+    <span className={`${size === "small" ? "text-md" : "text-lg"} font-small text-gray-600 dark:text-gray-300`}>{name[0]}</span>
   </div>
 }
 
